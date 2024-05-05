def2b2e9c90efa08bb8ad6ec5e48b13d736e7f7b
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-05-05 19:27:24 +0300

    Update ResultsAndRevenue.tsx

--- src/components/ResultsAndRevenue.tsx
+++ src/components/ResultsAndRevenue.tsx
@@ -10,11 +10,11 @@ const ResultsAndRevenue = () => {
         
         <div className='flex flex-col md:flex-row gap-[100px] mx-auto w-full md:w-[80%] p-6 border-t border-gray-400 m-10 mt-[80px]'>
             <div>
-                <h1 className='font-inter font-bold text-[72px] whitespace-nowrap text-black'>$2.5 M</h1>
+                <h1 className='font-inter font-bold text-[50px]  md:text-[72px] whitespace-nowrap text-black'>$2.5 M</h1>
                 <p className='font-inter font-bold text-[20px] text-black'>Generate sales</p>
             </div>
             <div>
-                <p className='font-inter font-medium text-[30px] w-full md:w-[80%] text-black'>Using Кирилица CRM is one of the best decisions we’ve ever made. We’ve seen our annual revenue explode, and the outlook just keeps getting sunnier.</p>
+                <p className='font-inter font-medium text-[20px] md:text-[30px] w-full md:w-[80%] text-black'>Using Кирилица CRM is one of the best decisions we’ve ever made. We’ve seen our annual revenue explode, and the outlook just keeps getting sunnier.</p>
                 <div className='flex gap-5 pt-5'>
                     <img src="Oval.png" alt="" />
                     <div>
@@ -29,11 +29,11 @@ const ResultsAndRevenue = () => {
 
         <div className='flex flex-col md:flex-row  gap-[100px] w-full md:w-[80%] mx-auto p-6 border-t border-gray-400 m-10'>
             <div>
-                <h1 className='font-inter font-bold text-[72px] whitespace-nowrap text-black'>45%</h1>
+                <h1 className='font-inter font-bold text-[50px] md:text-[72px] whitespace-nowrap text-black'>45%</h1>
                 <p className='font-inter font-bold text-[20px] text-black'>Grew revenue</p>
             </div>
             <div className='flex flex-col w-full md:w-[67%] mx-auto'>
-                <p className='font-inter font-medium text-[30px] w-full md:w-[80%] text-black'>Yoora is created for sales people. It’s the kind of software that just works. I don’t have to try to make it work. It already does. It’s just perfect.</p>
+                <p className='font-inter font-medium text-[20px] md:text-[30px] w-full md:w-[80%] text-black'>Yoora is created for sales people. It’s the kind of software that just works. I don’t have to try to make it work. It already does. It’s just perfect.</p>
                 <div className='flex gap-5 pt-5'>
                     <img src="Oval2.png" alt="" />
                     <div>
