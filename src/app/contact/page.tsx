9dffe1c2b98c6608c571faace56122d8793d47c8
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-05-05 20:51:06 +0300

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,14 +2,7 @@
 const page = () => {
   return (
     <div>CONTACT</div>
-     <div className="flex flex-col md:flex-row gap-10 md:gap-0 bg-white justify-between w-[85%] mx-auto p-10 ">
-            <p className="font-inter font-medium lg:pb-0 pb-5 lg:pl-0 pl-5 text-black">Trusted by 1,000+ customers</p>
-            <img src="Google.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] " />
-            <img src="Atlassian.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px]"/>
-            <img src="Canon.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px]"/>
-            <img src="Walmart.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px]"/>
-            <img src="Amazon.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px]"/>
-        </div>
+     
   )
 }
 
