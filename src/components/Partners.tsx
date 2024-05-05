84a89892326be0b394dd4c887cbdc4bd857a6bbd
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-05-05 23:43:00 +0300

    Update Partners.tsx

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Partners = () => {
   return (
-    <section className='bg-white pb-[80px] pt-10'>
+    <section className='bg-white pb-[80px] pt-5 md:pt-10'>
     <div className="flex flex-col md:flex-row gap-10 md:gap-0 bg-white justify-between w-[85%] mx-auto p-10 ">
     <p className="font-inter font-medium lg:pb-0 pb-5 lg:pl-0 pl-5 text-black">Our clients and partners</p>
     <img src="Google.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] " />
