19a4fd26ffc97caf87114f98002716203c4401a6
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-05-08 12:21:41 +0300

    Add files via upload

---
+++ src/components/HeaderPricing.tsx
@@ -0,0 +1,99 @@
+
+
+const HeaderPricing = () => {
+  return (
+    <section className="bg-backgroundColor">
+        <div>
+        <div className="flex flex-col lg:flex-row py-10 gap-5 items-center w-[80%] mx-auto"> 
+            <h1 className="font-inter font-bold text-[40px] lg:text-[64px] text-black">Choose the right plan <span className="text-gray-400">for your businnes</span></h1>
+            <div className="w-full lg:w-[55%] mx-auto"><p className="font-inter text-[18px]">Built with love for growing businesses. Check out the Roooby CRM pricing grid below. Switch between plans at any time.</p></div>
+        </div>
+        <div className="flex flex-col lg:flex-row gap-2 p-5 w-full lg:w-[90%] mx-auto pb-[80px]">
+            <div className="bg-white rounded-md hover:scale-105 duration-300">
+                <div className="p-5">
+                <h1 className="text-black font-inter font-bold text-[24px]">Starter</h1>
+                <p className="font-inter text-black text-[16px] px-2">Get organized and set up simple sales processes lorem ipsum</p>
+                <p className="font-inter text-black font-bold text-[48px]">$8,90 <span className="font-inter text-[16px] text-gray-400">/month</span></p>
+                <button className="border hover:bg-customBlue hover:text-white duration-300 rounded-lg border-gray-300 font-inter font-bold text-[18px] p-2 px-10">Try for Free</button>
+                <p className="font-inter text-gray-400">Free 14-day trial. No credit card required.</p>
+                </div>
+                
+                    <ul className="font-inter text-[16px] p-5 text-black border-t border-gray-300 flex flex-col gap-3">
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 2 team members</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 1,000 Contacts & Companies</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 5 Campaign Workflows</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 10 Nodes Per Campaign</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 5 Automation Rules (Triggers)</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 3 Plugins/Integrations</li>
+                        <li className="flex gap-2 line-through opacity-30"><img src="Ticked.png" alt="" /> Marketing Automation	</li>
+                        <li className="flex gap-2 line-through opacity-30"><img src="Ticked.png" alt="" /> Custom Deal Tracks</li>
+                        <li className="flex gap-2 line-through opacity-30"><img src="Ticked.png" alt="" /> Automated Voicemails	</li>
+                        <li className="flex gap-2 line-through opacity-30"><img src="Ticked.png" alt="" /> Post-call Automation	</li>
+
+                    </ul>
+                
+            </div>
+            <div className="bg-white rounded-md hover:scale-105 duration-300">
+                <div className="p-5">
+                <h1 className="text-black font-inter font-bold text-[24px]">Professional <span className="text-customGreen text-[15px] bg-green-100 p-1" >  Recommended</span></h1>
+                <p className="font-inter text-black text-[16px] px-2">Everything you need to boost performance and revenue lorem ipsum</p>
+                <p className="font-inter text-black font-bold text-[48px]">$29,90 <span className="font-inter text-[16px] text-gray-400">/month</span></p>
+                <button className="border hover:bg-customBlue hover:text-white duration-300 rounded-lg border-gray-300 font-inter font-bold text-[18px] p-2 px-10">Try for Free</button>
+                <p className="font-inter text-gray-400">Free 14-day trial. No credit card required.</p>
+                </div>
+                
+                    <ul className="font-inter text-[16px] p-5 text-black border-t border-gray-300 flex flex-col gap-3">
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 10 team members</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 2,500 Contacts & Companies</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 5 Campaign Workflows</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 10 Nodes Per Campaign</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 5 Automation Rules (Triggers)</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 3 Plugins/Integrations</li>
+                        <li className="flex gap-2 "><img src="Ticked.png" alt="" /> Marketing Automation	</li>
+                        <li className="flex gap-2 "><img src="Ticked.png" alt="" /> Custom Deal Tracks</li>
+                        <li className="flex gap-2 line-through opacity-30"><img src="Ticked.png" alt="" /> Automated Voicemails	</li>
+                        <li className="flex gap-2 line-through opacity-30"><img src="Ticked.png" alt="" /> Post-call Automation	</li>
+
+                    </ul>
+                
+            </div>
+            <div className="bg-white rounded-md hover:scale-105 duration-300">
+                <div className="p-5">
+                <h1 className="text-black font-inter font-bold text-[24px]">Enterprise</h1>
+                <p className="font-inter text-black text-[16px] px-2">Customize without limits and access unrivaled support lorem ipsum</p>
+                <p className="font-inter text-black font-bold text-[48px]">$39,90 <span className="font-inter text-[16px] text-gray-400">/month</span></p>
+                <button className="border hover:bg-customBlue hover:text-white duration-300 rounded-lg border-gray-300 font-inter font-bold text-[18px] p-2 px-10">Try for Free</button>
+                <p className="font-inter text-gray-400">Free 14-day trial. No credit card required.</p>
+                </div>
+                
+                    <ul className="font-inter text-[16px] p-5 text-black border-t border-gray-300 flex flex-col gap-3">
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> Unlimited team members</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 5,000 Contacts & Companies</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 25 Campaign Workflows</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 20 Nodes Per Campaign</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 15 Automation Rules (Triggers)</li>
+                        <li className="flex gap-2"><img src="Ticked.png" alt="" /> 10 Plugins/Integrations</li>
+                        <li className="flex gap-2 "><img src="Ticked.png" alt="" /> Marketing Automation	</li>
+                        <li className="flex gap-2 "><img src="Ticked.png" alt="" /> Custom Deal Tracks</li>
+                        <li className="flex gap-2 "><img src="Ticked.png" alt="" /> Automated Voicemails	</li>
+                        <li className="flex gap-2 "><img src="Ticked.png" alt="" /> Post-call Automation	</li>
+                    </ul>
+
+                
+            </div>
+
+
+        </div>
+        <div className="flex gap-8 justify-center p-5 py-[50px] border-t border-black w-[87%] mx-auto">
+            <h1 className="font-inter font-bold text-black text-[32px] ">Need help choosing the right plan?</h1>
+            <button className="font-inter text-black text-[18px] hover:text-white hover:bg-customBlue duration-300 border p-2 px-4 bg-white rounded-lg">Contact sales</button>
+            <button className="font-inter text-black text-[18px] hover:text-white hover:bg-customBlue duration-300 border p-2 px-4 bg-white rounded-lg">Compare plans</button>
+        </div>
+
+
+        </div>
+    </section>
+  )
+}
+
+export default HeaderPricing
---
+++ src/components/Questions.tsx
@@ -0,0 +1,111 @@
+"use client";
+import { useState } from 'react';
+
+const Questions = () => {
+    const [isVisible1, setIsVisible1] = useState(false);
+    const [isVisible2, setIsVisible2] = useState(false);
+    const [isVisible3, setIsVisible3] = useState(false);
+    const [isVisible4, setIsVisible4] = useState(false);
+    const [isVisible5, setIsVisible5] = useState(false);
+    
+
+    return (
+        <section className='bg-white p-5'>
+            <div className='w-full lg:w-[60%] p-5 pl-[100px]'>
+                <h1 className='font-inter font-bold text-black text-[64px]'>Frequently asked questions</h1>
+            </div>
+            <div className='w-full lg:w-[80%] ml-auto  p-5 px-10'>
+                <div className='flex flex-col gap-5 border-t  border-gray-500 p-2 py-5 '>
+                    <div className='flex items-center'>
+                        <h1 className='font-inter font-medium text-[32px]'>How Does Roooby CRM Compare To The Competition?</h1>
+                        <button
+                            className="text-white font-bold py-2 px-4 rounded "
+                            onClick={() => setIsVisible1(!isVisible1)}
+                        >
+                            <img src="Plus.png" alt="Toggle Button"  className={`transition-transform duration-300 ${isVisible1 ? 'rotate-45' : 'rotate-0'}`} />
+                        </button>
+                    </div>
+                    {isVisible1 && (
+                        <p className=" p-2 text-gray-500 ">
+                            Rooby CRM offers the most affordable, comprehensive platform to manage the entire customer journey, from generating leads, to closing deals and supporting your customers. With a full suite of CRM, sales enablement, marketing automation, and help desk/customer support capabilities, it’s all you need to manage the entire customer lifecycle. Most of our competitors offer one or two of these functions as separate products, leaving you with the headache of integrating them and increasing the overall price point.
+                        </p>
+                    )}
+                </div>
+            </div>
+            <div className='w-full lg:w-[80%] ml-auto  p-5 px-10'>
+                <div className='flex flex-col gap-5 border-t  border-gray-500 p-2 py-5 '>
+                    <div className='flex items-center'>
+                        <h1 className='font-inter font-medium text-[32px]'>Can I Change Plans Or Cancel My Subscription At Any Time?</h1>
+                        <button
+                            className="text-white font-bold py-2 px-4 rounded "
+                            onClick={() => setIsVisible2(!isVisible2)}
+                        >
+                            <img src="Plus.png" alt="Toggle Button"  className={`transition-transform duration-300 ${isVisible2 ? 'rotate-45' : 'rotate-0'}`} />
+                        </button>
+                    </div>
+                    {isVisible2 && (
+                        <p className=" p-2 text-gray-500 ">
+                           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate ducimus impedit ea molestias quae sed natus doloribus deleniti perspiciatis optio iusto, vel aut, quisquam dolor enim qui minus, id accusantium?
+                        </p>
+                    )}
+                </div>
+            </div>
+            <div className='w-full lg:w-[80%] ml-auto  p-5 px-10'>
+                <div className='flex flex-col gap-5 border-t  border-gray-500 p-2 py-5 '>
+                    <div className='flex items-center'>
+                        <h1 className='font-inter font-medium text-[32px]'>How Secure Is My Data With Roooby CRM?</h1>
+                        <button
+                            className="text-white font-bold py-2 px-4 rounded "
+                            onClick={() => setIsVisible3(!isVisible3)}
+                        >
+                            <img src="Plus.png" alt="Toggle Button"  className={`transition-transform duration-300 ${isVisible3 ? 'rotate-45' : 'rotate-0'}`} />
+                        </button>
+                    </div>
+                    {isVisible3 && (
+                        <p className=" p-2 text-gray-500 ">
+                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores neque, dolor explicabo fugiat eos dignissimos aliquam nemo suscipit facilis excepturi, blanditiis itaque aperiam nisi! Molestias libero praesentium error inventore eos!
+                        </p>
+                    )}
+                </div>
+            </div>
+            <div className='w-full lg:w-[80%] ml-auto  p-5 px-10'>
+                <div className='flex flex-col gap-5 border-t  border-gray-500 p-2 py-5 '>
+                    <div className='flex items-center'>
+                        <h1 className='font-inter font-medium text-[32px]'>What Is The Uptime Guarantee?</h1>
+                        <button
+                            className="text-white font-bold py-2 px-4 rounded "
+                            onClick={() => setIsVisible4(!isVisible4)}
+                        >
+                            <img src="Plus.png" alt="Toggle Button"  className={`transition-transform duration-300 ${isVisible4 ? 'rotate-45' : 'rotate-0'}`} />
+                        </button>
+                    </div>
+                    {isVisible4 && (
+                        <p className=" p-2 text-gray-500 ">
+                           Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem libero sequi voluptatum reiciendis modi facilis eaque excepturi. Architecto, consectetur. Excepturi beatae ipsum, voluptate alias officiis fugiat reiciendis eius modi ad?
+                        </p>
+                    )}
+                </div>
+            </div>
+            <div className='w-full lg:w-[80%] ml-auto  p-5 px-10'>
+                <div className='flex flex-col gap-5 border-t  border-gray-500 p-2 py-5 '>
+                    <div className='flex items-center'>
+                        <h1 className='font-inter font-medium text-[32px]'>How Can I Add More Emails To My Account?</h1>
+                        <button
+                            className="text-white font-bold py-2 px-4 rounded "
+                            onClick={() => setIsVisible5(!isVisible5)}
+                        >
+                            <img src="Plus.png" alt="Toggle Button"  className={`transition-transform duration-300 ${isVisible5 ? 'rotate-45' : 'rotate-0'}`} />
+                        </button>
+                    </div>
+                    {isVisible5 && (
+                        <p className=" p-2 text-gray-500 ">
+                           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic provident est, explicabo ex ducimus unde molestiae asperiores id tempora aperiam optio necessitatibus adipisci fugiat, cumque, dolorum reiciendis non. Rem, dolores!
+                        </p>
+                    )}
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Questions;
