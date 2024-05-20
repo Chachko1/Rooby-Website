a6ad0e84b67300332039a66f4209ac06793c5853
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-05-20 11:04:31 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="container mx-auto">
+    <html lang="en">
       <body className={`${inter.className} `}>
         <Navbar/>
         {children}
