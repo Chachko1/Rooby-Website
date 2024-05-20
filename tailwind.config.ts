84f6c5422451a8cfdf8c1b9a399f7517ae423e48
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-05-20 11:00:56 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,30 +1,39 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors:{
-        customBlue:"#150050",
-        backgroundColor:"#F4F4F4",
-        customYellow:"#FFE377",
-        customGreen:"#00CC61"
+      colors: {
+        customBlue: '#150050',
+        backgroundColor: '#F4F4F4',
+        customYellow: '#FFE377',
+        customGreen: '#00CC61',
       },
       fontFamily: {
         inter: ['Inter', 'sans-serif'],
-        
       },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+        screens: {
+          sm: '100%',
+          md: '100%',
+          lg: '1024px',
+          xl: '1024px',
+        },
       },
     },
   },
   plugins: [],
 };
+
 export default config;
