be6b15a94e66d2cc86894dd61b1e6e9ab8fddd07
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-05-20 11:10:14 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="max-w-[1024px]">
       <body className={`${inter.className} `}>
         <Navbar/>
         {children}
