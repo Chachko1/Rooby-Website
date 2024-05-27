ebba21e97988dde983f3f324964175a8c4f95af6
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-05-27 15:30:44 +0300

    Update Partners.tsx

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -2,16 +2,18 @@ import React from 'react'
 
 const Partners = () => {
   return (
-    <section className='bg-white pb-10 md:pb-[80px] pt-10'>
+    <div className='bg-white'>
+    <section className='bg-white pb-10 md:pb-[80px] pt-10 max-w-[1440px] mx-auto'>
     <div className="flex flex-col lg:flex-row gap-10 lg:gap-0 bg-white justify-between w-[85%] mx-auto p-10 ">
     <p className="font-inter font-medium lg:pb-0 pb-5 lg:pl-0 pl-5 text-black">Our clients and partners</p>
-    <img src="Google.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] " />
-    <img src="Atlassian.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px]"/>
-    <img src="Canon.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px]"/>
-    <img src="Walmart.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px]"/>
-    <img src="Amazon.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px]"/>
+    <img src="Google.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110" />
+    <img src="Atlassian.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
+    <img src="Canon.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
+    <img src="Walmart.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
+    <img src="Amazon.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
     </div>
     </section>
+    </div>
   )
 }
 
