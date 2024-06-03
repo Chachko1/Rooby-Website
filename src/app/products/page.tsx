7dc024e2cbc0ca7990d03f0982762b2bd4a7d61b
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-06-03 13:01:38 +0300

    Update page.tsx

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,7 +2,7 @@
 
 const page = () => {
   return (
-    <div className="text-black text-3xl p-10  bg-backgroundColor" >
+    <div className="text-black text-5xl p-10 text-center  bg-backgroundColor" >
       <p className="max-w-[1440px] mx-auto" >Coming soon..</p></div>
   )
 }
