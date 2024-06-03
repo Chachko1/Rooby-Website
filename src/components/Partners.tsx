06ed7a660c3b497a051e21d0488203fb307b3d64
Alexander Vatev <121901814+Chachko1@users.noreply.github.com>
2024-06-03 13:04:49 +0300

    Update Partners.tsx

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -6,11 +6,11 @@ const Partners = () => {
     <section className='bg-white pb-10 md:pb-[80px] pt-10 max-w-[1440px] mx-auto'>
     <div className="flex flex-col lg:flex-row gap-10 lg:gap-0 bg-white justify-between w-[85%] mx-auto p-10 ">
     <p className="font-inter font-medium lg:pb-0 pb-5 lg:pl-0 pl-5 text-black">Our clients and partners</p>
-    <img src="Google.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110" />
-    <img src="Atlassian.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
-    <img src="Canon.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
-    <img src="Walmart.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
-    <img src="Amazon.png" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
+    <img src="Google.svg" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110" />
+    <img src="Atlassian.svg" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
+    <img src="Canon.svg" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
+    <img src="Walmart.svg" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
+    <img src="Amazon.svg" alt="" className="w-[200px] h-[40px] lg:w-[100px] lg:h-[35px] transform scale-x-110"/>
     </div>
     </section>
     </div>
